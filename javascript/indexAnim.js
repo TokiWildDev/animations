f0a4215cbf8a9bb0a5f2dfc3358382d3b0f446f2
TokiWildDev <tokiworldwilddev@gmail.com>
2022-08-16 19:56:03 +0200

    misen place deuxiéme page anim

---
+++ javascript/indexAnim.js
@@ -0,0 +1,8 @@
+const load = document.getElementsByClassName('rond')
+
+
+    load[0].addEventListener('click',()=>{
+
+        console.log('hello');
+        window.location.href = 'anim2.html';
+    })
